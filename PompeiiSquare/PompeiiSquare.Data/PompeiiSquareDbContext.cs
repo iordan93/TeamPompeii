30c8c9e6ae264c9ef617328d2f5322021a4bb9ef
Ventsislav Yordanov <venciity@abv.bg>
2015-05-25 22:10:09 +0300

    add common logic for venues and update context

--- PompeiiSquare/PompeiiSquare.Data/PompeiiSquareDbContext.cs
+++ PompeiiSquare/PompeiiSquare.Data/PompeiiSquareDbContext.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNet.Identity.EntityFramework;
     using PompeiiSquare.Models;
+    using System.Data.Entity;
 
     public class PompeiiSquareDbContext : IdentityDbContext<User>
     {
@@ -10,6 +11,26 @@
         {
         }
 
+        public IDbSet<Venue> Venues { get; set; }
+
+        public IDbSet<Event> Events { get; set; }
+
+        public IDbSet<Tag> Tags { get; set; }
+
+        public IDbSet<VenueGroup> VenueGroups { get; set; }
+
+        public IDbSet<EventCategory> EventCategories { get; set; }
+
+        public IDbSet<Checkin> Checkins { get; set; }
+
+        public IDbSet<Gender> Gender { get; set; }
+
+        public IDbSet<Tip> Tips { get; set; }
+
+        public IDbSet<OpenHours> OpenHours { get; set; }
+
+        public IDbSet<Photo> Photos { get; set; }
+        
         public static PompeiiSquareDbContext Create()
         {
             return new PompeiiSquareDbContext();
