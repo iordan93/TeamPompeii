43c04c2169e6f10bec2cf14f9ac90cdd18e36b08
Yordan Darakchiev <iordan93@gmail.com>
2015-05-30 15:50:07 +0300

    Brought back authorization for venue admins

--- PompeiiSquare/PompeiiSquare.Server/Areas/VenueAdministrator/Controllers/BaseVenueAdministratorController.cs
+++ PompeiiSquare/PompeiiSquare.Server/Areas/VenueAdministrator/Controllers/BaseVenueAdministratorController.cs
@@ -9,7 +9,7 @@
     using PompeiiSquare.Data.UnitOfWork;
     using PompeiiSquare.Server.Controllers;
 
-    //[Authorize(Roles = "VenueAdministrator")] TODO: Uncomment this later!
+    [Authorize(Roles = "VenueAdministrator")]
     public abstract class BaseVenueAdministratorController : BaseController
     {
         protected BaseVenueAdministratorController(IPompeiiSquareData data)
