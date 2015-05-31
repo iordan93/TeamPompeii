c69c3c6230a369c757d87688d7cfed564d4d42e6
Ventsislav Yordanov <venciity@abv.bg>
2015-06-01 00:43:33 +0300

    add VenueEditBindingModel.cs and Edit View for venues

---
+++ PompeiiSquare/PompeiiSquare.Server/Areas/VenueAdministrator/Models/VenueEditBindingModel.cs
@@ -0,0 +1,42 @@
+﻿namespace PompeiiSquare.Server.Areas.VenueAdministrator.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Entity.Spatial;
+
+    public class VenueEditBindingModel
+    {
+        public VenueEditBindingModel()
+        {
+            this.OpenHours = new List<OpenHoursBindingModel>(1) { new OpenHoursBindingModel() };
+        }
+
+        public int? Id { get; set; }
+
+        [Required, MinLength(2, ErrorMessage = "The name must be at least 2 symbols long.")]
+        public string Name { get; set; }
+
+        [Required, MinLength(10, ErrorMessage = "The address must be at least 10 symbols long.")]
+        public string Address { get; set; }
+
+        [Required, MinLength(2, ErrorMessage = "The contact must be at least 2 symbols long.")]
+        public string Contact { get; set; }
+
+        public DbGeography Location { get; set; }
+
+        [Range(0, 4)]
+        public int PriceTier { get; set; }
+
+        public IList<int> Groups { get; set; }
+
+        [Display(Name = "Open hours")]
+        public IList<OpenHoursBindingModel> OpenHours { get; set; }
+
+        [Required, MinLength(10, ErrorMessage = "The description must be at least 10 symbols long.")]
+        public string Description { get; set; }
+
+        public string Tags { get; set; }
+
+        // TODO: Add photo?
+    }
+}
