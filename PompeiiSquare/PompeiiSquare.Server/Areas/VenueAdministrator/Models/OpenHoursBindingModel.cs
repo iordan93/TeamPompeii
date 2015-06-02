99ff3ab977935951c9c65ee0b5140bc48330ec6e
Ventsislav Yordanov <venciity@abv.bg>
2015-06-03 01:35:11 +0300

    add validation for OpenHoursBindingModel

--- PompeiiSquare/PompeiiSquare.Server/Areas/VenueAdministrator/Models/OpenHoursBindingModel.cs
+++ PompeiiSquare/PompeiiSquare.Server/Areas/VenueAdministrator/Models/OpenHoursBindingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace PompeiiSquare.Server.Areas.VenueAdministrator.Models
     {
         public string Weekday { get; set; }
 
+        [RegularExpression(@"^(\d{1,2}:\d{2}\s*-\s*\d{1,2}:\d{2})$", ErrorMessage = "Invalid hours input. The input must be in format: 00:00 - 24:00")]
         public string Hours { get; set; }
     }
 }
