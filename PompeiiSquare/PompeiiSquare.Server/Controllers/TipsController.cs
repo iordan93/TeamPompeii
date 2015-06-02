1d3645ed78101e14aedc74157e141a6737055875
Ventsislav Yordanov <venciity@abv.bg>
2015-06-02 22:31:32 +0300

    add base logic for creating new tip

---
+++ PompeiiSquare/PompeiiSquare.Server/Controllers/TipsController.cs
@@ -0,0 +1,33 @@
+﻿namespace PompeiiSquare.Server.Controllers
+{
+    using PompeiiSquare.Data.UnitOfWork;
+    using PompeiiSquare.Server.Models;
+    using System.Net;
+    using System.Web.Mvc;
+
+    public class TipsController : BaseController
+    {
+        public TipsController(IPompeiiSquareData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.PartialView();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Create(TipBindingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save Tip to DB
+                return new HttpStatusCodeResult(HttpStatusCode.Created);
+            }
+
+            return Content(""); // TODO: Redirect
+        }
+    }
+}
