afcc32c5ceea4d75cd17d2981f9d00fcb48e55f2
Ventsislav Yordanov <venciity@abv.bg>
2015-06-03 21:44:25 +0300

    .

---
+++ PompeiiSquare/PompeiiSquare.Server/Models/TipBindingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PompeiiSquare.Server.Models
+{
+    public class TipBindingModel
+    {
+        public int venueId { get; set; }
+
+        public string Content { get; set; }
+    }
+}
