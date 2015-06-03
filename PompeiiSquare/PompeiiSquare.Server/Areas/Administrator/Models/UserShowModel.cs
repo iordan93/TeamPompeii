29033dc370287fe435174c5c8325af1771513fcd
Ventsislav Yordanov <venciity@abv.bg>
2015-06-03 19:04:37 +0300

    add the new Models

---
+++ PompeiiSquare/PompeiiSquare.Server/Areas/Administrator/Models/UserShowModel.cs
@@ -0,0 +1,21 @@
+﻿using PompeiiSquare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PompeiiSquare.Server.Areas.Administrator.Models
+{
+    public class UserShowModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string HomeCity { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public virtual string Email { get; set; }
+    }
+}
