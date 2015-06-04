b89111d5400c1926ad2ef5cb9e78f52b53c779db
Yordan Darakchiev <iordan93@gmail.com>
2015-06-04 15:31:20 +0300

    Implemented editing and deleting venues

--- PompeiiSquare/PompeiiSquare.Server/Controllers/TipsController.cs
+++ PompeiiSquare/PompeiiSquare.Server/Controllers/TipsController.cs
@@ -67,5 +67,87 @@
             this.Data.SaveChanges();
             return this.PartialView("_TipDetails", tip);
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var tip = this.Data.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (tip.User != this.UserProfile)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You cannot edit a tip which is not yours.");
+            }
+
+            return View(new TipBindingModel() { Id = tip.Id, Content = tip.Text, venueId = tip.VenueId });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Update(TipBindingModel model)
+        {
+            var tipDb = this.Data.Tips.Find(model.Id);
+            if (tipDb == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (tipDb.User != this.UserProfile)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You cannot edit a tip which is not yours.");
+            }
+
+            tipDb.Text = model.Content;
+            this.Data.SaveChanges();
+
+            if (model.Photo != null)
+            {
+                var path = DropboxRepository.Upload(model.Photo.FileName, this.UserProfile.UserName, model.Photo.InputStream);
+                var photo = new Photo() { Path = path, Author = this.UserProfile, CreatedAt = DateTime.Now };
+                tipDb.Photo = photo;
+                this.Data.SaveChanges();
+            }
+
+            return RedirectToAction("ViewDetails", "Venues", new { id = model.venueId });
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var tip = this.Data.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (tip.User != this.UserProfile)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You cannot delete a tip which is not yours.");
+            }
+
+            return View(new TipBindingModel() { Id = tip.Id, Content = tip.Text, venueId = tip.VenueId });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Remove(TipBindingModel model)
+        {
+            var tipDb = this.Data.Tips.Find(model.Id);
+            if (tipDb == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (tipDb.User != this.UserProfile)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You cannot edit a tip which is not yours.");
+            }
+
+            this.Data.Tips.Remove(model.Id);
+            this.Data.SaveChanges();
+
+            return RedirectToAction("ViewDetails", "Venues", new { id = model.venueId });
+        }
     }
 }
--- PompeiiSquare/PompeiiSquare.Server/Models/TipBindingModel.cs
+++ PompeiiSquare/PompeiiSquare.Server/Models/TipBindingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace PompeiiSquare.Server.Models
 {
     public class TipBindingModel
     {
+        public int Id { get; set; }
+
         public int venueId { get; set; }
 
+        [Required, MinLength(3, ErrorMessage = "The tip text must be at least 3 symbols long")]
         public string Content { get; set; }
 
         public HttpPostedFileBase Photo { get; set; }
